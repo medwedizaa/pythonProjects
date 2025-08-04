ef9424b7fbb66d53ee25d19a2ca577a38ef58215
Aleksandra Batanova <medwedizaa@gmail.com>
2025-08-04 20:00:59 +0200

    Improved project #26

--- 26_nato_alphabet_spelling/main.py
+++ 26_nato_alphabet_spelling/main.py
@@ -1,8 +1,17 @@
 import pandas as pd
 
+
+def get_spelled_word():
+    name = input("Enter a word to be spelled: ")
+    try:
+        return [alphabet_dict[x.capitalize()] for x in name]
+    except KeyError:
+        print("Sorry, only letters in th alphabet please")
+        return get_spelled_word()
+
+
 df = pd.read_csv('nato_phonetic_alphabet.csv')
 alphabet_dict = {row['letter']: row['code'] for (index, row) in df.iterrows()}
 
-name = input("Enter a name to be spelled: ")
-result = [alphabet_dict[x.capitalize()] for x in name]
+result = get_spelled_word()
 print(result)
